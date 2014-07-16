e97ae5d23af1667986b9df78c642dbf69f44dc59
Luis Gago Casas <luisgago@lagc-peru.com>
2014-07-16 00:43:39 -0500

    Reporte Cursos primera parte

--- componentes/reporte/inicio.php
+++ componentes/reporte/inicio.php
@@ -1,3 +1,4 @@
 <?php include "componentes/reporte/class.contenidos.php";
-if(isset($_GET['id']) && $_GET['buscar']) { Reporte::inicio($_GET['buscar']); }
+if (!isset($_GET['id'])) { Reporte::inicio(); }
+if(isset($_GET['id']) && $_GET['reportecurso']) { Reporte::reportecurso($_GET['id'], $_GET['reportecurso']); }
 ?>
