d599deef1be1891d2778bf8142c813f572d40bcc
Luis Gago Casas <luisgago@lagc-peru.com>
2014-07-18 01:31:25 -0500

    Sedes reporte web y excel

--- componentes/reporte/class.contenidos.php
+++ componentes/reporte/class.contenidos.php
@@ -35,7 +35,7 @@ class Reporte{
 	static function reportesedes($val1, $val2){
 		$respconfig = mysql_query("select * from configuracion"); $config = mysql_fetch_array($respconfig);
 		$respsedes = mysql_query("select * from com_sedes where sede_id='".$val1."'"); $sedes = mysql_fetch_array($respsedes);
-		$respcursos = mysql_query("select * from com_cursos where curso_id='".$sedes['ids']."'"); $cursos = mysql_fetch_array($respcursos); ?> ?>
+		$respcursos1 = mysql_query("select * from com_cursos where sede_id='".$sedes['sede_id']."'"); $cursos1 = mysql_fetch_array($respcursos1); ?>
 		<div class="tlcabecera">
 			<a href="?lagc=reporte" title="Lista de Cursos" class="menucompo">
 				<img src="plantillas/default/img/lista.png"><b>Sedes y Cursos</b></a>
@@ -44,7 +44,89 @@ class Reporte{
 			<a href="?lagc=reporte&id=seguros" title="Seguros" class="menucompo">
 				<img src="plantillas/default/img/lista.png">Seguros</a>
 		</div>
-		<div style="text-align: right;"><a href="componentes/reporte/exportar.php?idcurso=<?=$_GET['id']; ?>&sede=<?=Reporte::nombresede($cursos['sede_id']); ?>&curso=<?=$cursos['curso_nombre']; ?>">Exportar</a></div>
+		<div style="text-align: right;"><a href="componentes/reporte/exportar.php?que=sede&idsede=<?=$_GET['id']; ?>&sede=<?=Reporte::nombresede($cursos1['sede_id']); ?>">Exportar</a></div>
+		<div class="grid grid-pad">
+		    <div class="col-1-4">
+		    	<img src="utilidades/imagenes/<?=$config['logo']; ?>" class="imgrepor">
+		    </div>
+		    <div class="col-1-4">
+			    </br></br><?=$config['nombreapp']; ?>
+		    </div>
+		    <div class="col-1-4">
+
+		    </div>
+		    <div class="col-1-4">
+
+		    </div>
+		</div>
+		<div class="grid grid-pad">
+		    <div class="col-1-4 rptasunto" style="text-align: right;">
+		    	SEDE:
+		    </div>
+		    <div class="col-1-4">
+			    <?=Reporte::nombresede($sedes['sede_id']); ?>
+		    </div>
+		    <div class="col-1-4">
+
+		    </div>
+		    <div class="col-1-4">
+
+		    </div>
+		</div>
+		<div class="grid grid-pad">
+		    <div class="col-1-4 rptasunto" style="text-align: right;">
+		    	CURSOS:
+		    </div>
+		    <div class="col-1-4">
+			    <?=Reporte::nombrecursos($sedes['sede_id'], true); ?>
+		    </div>
+		    <div class="col-1-4">
+
+		    </div>
+		    <div class="col-1-4">
+
+		    </div>
+		</div>
+		<div class="grid grid-pad">
+			<div class="col-1-6 rptasunto">DNI</div>
+			<div class="col-1-6 rptasunto">NOMBRES</div>
+			<div class="col-1-6 rptasunto">APELLIDOS</div>
+			<div class="col-1-6 rptasunto">NOMBRE DEL CURSO</div>
+			<div class="col-1-6 rptasunto">FECHA INICIO</div>
+			<div class="col-1-6 rptasunto">FECHA FIN</div>
+		</div>
+		<div class="grid grid-pad">
+			<?php
+			$respcursos = mysql_query("select * from com_cursos where sede_id='".$sedes['sede_id']."'");
+			while ($cursos = mysql_fetch_array($respcursos)){
+				$respcursoasg = mysql_query("select * from com_curso_asignar where id_curso='".$cursos['curso_id']."'");
+				while($asigcurso = mysql_fetch_array($respcursoasg)) {
+					$respersonal = mysql_query("select * from usuarios where id='".$asigcurso['id_usuario']."'");
+					$personal = mysql_fetch_array($respersonal);
+				    echo "<div class=\"col-1-6\">".$personal['dni']."</div>";
+				    echo "<div class=\"col-1-6\">".$personal['nombres']."</div>";
+				    echo "<div class=\"col-1-6\">".$personal['apellidop']." ".$personal['apellidom']."</div>";
+				    echo "<div class=\"col-1-6\">".Reporte::nombrecursos($cursos['curso_id'], false)."</div>";
+				    echo "<div class=\"col-1-6\">".$asigcurso['fechainicio']."</div>";
+				    echo "<div class=\"col-1-6\">".$asigcurso['fechafin']."</div>";
+				}
+			}
+			?>
+		</div>
+	<?php
+	}
+	static function reportecurso($val1, $val2){
+		$respconfig = mysql_query("select * from configuracion"); $config = mysql_fetch_array($respconfig);
+		$respcursos = mysql_query("select * from com_cursos where curso_id='".$_GET['id']."'"); $cursos = mysql_fetch_array($respcursos); ?>
+		<div class="tlcabecera">
+			<a href="?lagc=reporte" title="Lista de Cursos" class="menucompo">
+				<img src="plantillas/default/img/lista.png"><b>Sedes y Cursos</b></a>
+			<a href="?lagc=reporte&id=examenes" title="Exámenes" class="menucompo">
+				<img src="plantillas/default/img/lista.png">Exámenes</a>
+			<a href="?lagc=reporte&id=seguros" title="Seguros" class="menucompo">
+				<img src="plantillas/default/img/lista.png">Seguros</a>
+		</div>
+		<div style="text-align: right;"><a href="componentes/reporte/exportar.php?que=curso&idcurso=<?=$_GET['id']; ?>&sede=<?=Reporte::nombresede($cursos['sede_id']); ?>&curso=<?=$cursos['curso_nombre']; ?>">Exportar</a></div>
 		<div class="grid grid-pad">
 		    <div class="col-1-4">
 		    	<img src="utilidades/imagenes/<?=$config['logo']; ?>" class="imgrepor">
@@ -109,81 +191,28 @@ class Reporte{
 		</div>
 	<?php
 	}
-	static function reportecurso($val1, $val2){
-		$respconfig = mysql_query("select * from configuracion"); $config = mysql_fetch_array($respconfig);
-		$respcursos = mysql_query("select * from com_cursos where curso_id='".$_GET['id']."'"); $cursos = mysql_fetch_array($respcursos); ?>
-		<div class="tlcabecera">
-			<a href="?lagc=reporte" title="Lista de Cursos" class="menucompo">
-				<img src="plantillas/default/img/lista.png"><b>Sedes y Cursos</b></a>
-			<a href="?lagc=reporte&id=examenes" title="Exámenes" class="menucompo">
-				<img src="plantillas/default/img/lista.png">Exámenes</a>
-			<a href="?lagc=reporte&id=seguros" title="Seguros" class="menucompo">
-				<img src="plantillas/default/img/lista.png">Seguros</a>
-		</div>
-		<div style="text-align: right;"><a href="componentes/reporte/exportar.php?idcurso=<?=$_GET['id']; ?>&sede=<?=Reporte::nombresede($cursos['sede_id']); ?>&curso=<?=$cursos['curso_nombre']; ?>">Exportar</a></div>
-		<div class="grid grid-pad">
-		    <div class="col-1-4">
-		    	<img src="utilidades/imagenes/<?=$config['logo']; ?>" class="imgrepor">
-		    </div>
-		    <div class="col-1-4">
-			    </br></br><?=$config['nombreapp']; ?>
-		    </div>
-		    <div class="col-1-4">
-
-		    </div>
-		    <div class="col-1-4">
-
-		    </div>
-		</div>
-		<div class="grid grid-pad">
-		    <div class="col-1-4 rptasunto" style="text-align: right;">
-		    	SEDE:
-		    </div>
-		    <div class="col-1-4">
-			    <?=Reporte::nombresede($cursos['sede_id']); ?>
-		    </div>
-		    <div class="col-1-4">
-
-		    </div>
-		    <div class="col-1-4">
-
-		    </div>
-		</div>
-		<div class="grid grid-pad">
-		    <div class="col-1-4 rptasunto" style="text-align: right;">
-		    	CURSO:
-		    </div>
-		    <div class="col-1-4">
-			    <?=$cursos['curso_nombre']; ?>
-		    </div>
-		    <div class="col-1-4">
-
-		    </div>
-		    <div class="col-1-4">
-
-		    </div>
-		</div>
-		<div class="grid grid-pad">
-			<div class="col-1-5 rptasunto">DNI</div>
-			<div class="col-1-5 rptasunto">NOMBRES</div>
-			<div class="col-1-5 rptasunto">APELLIDOS</div>
-			<div class="col-1-5 rptasunto">FECHA INICIO</div>
-			<div class="col-1-5 rptasunto">FECHA FIN</div>
-		</div>
-		<div class="grid grid-pad">
-			<?php $respcursoasg = mysql_query("select * from com_curso_asignar where id_curso='".$cursos['curso_id']."'");
-			while($asigcurso = mysql_fetch_array($respcursoasg)) {
-				$respersonal = mysql_query("select * from usuarios where id='".$asigcurso['id_usuario']."'");
-				$personal = mysql_fetch_array($respersonal);
-			    echo "<div class=\"col-1-5\">".$personal['dni']."</div>";
-			    echo "<div class=\"col-1-5\">".$personal['nombres']."</div>";
-			    echo "<div class=\"col-1-5\">".$personal['apellidop']." ".$personal['apellidom']."</div>";
-			    echo "<div class=\"col-1-5\">".$asigcurso['fechainicio']."</div>";
-			    echo "<div class=\"col-1-5\">".$asigcurso['fechafin']."</div>";
+	static function nombrecursos($val1, $val2){
+		if($val2==false){
+			$respcursos = mysql_query("select * from com_cursos where curso_id='".$val1."'");
+			$cursos = mysql_fetch_array($respcursos);
+			$final = $cursos['curso_nombre'];
+		}
+		else if($val2==true){
+			$respcursos = mysql_query("select * from com_cursos where sede_id='".$val1."'");
+			$rows = mysql_num_rows($respcursos);
+			$i=1;
+			while($cursos = mysql_fetch_array($respcursos)){
+				$final .= $cursos['curso_nombre'];
+				if($rows>$i){
+					$final .= ", ";
+				}
+				else if($rows==$i){
+					$final .="";
+				}
+				$i++;
 			}
-			?>
-		</div>
-	<?php
+		}
+		return $final;
 	}
 	static function nombresede($val1){
 		$respsede = mysql_query("select * from com_sedes where sede_id='".$val1."'"); $sede = mysql_fetch_array($respsede);
--- componentes/reporte/exportar.php
+++ componentes/reporte/exportar.php
@@ -1,28 +1,62 @@
 <?php
-include "../../config.php"; include "../../funciones/function.globales.php";
-	$salida_cvs = "Sede:,".$_GET['sede'].",";
-	$salida_cvs .= "\n";
-	$salida_cvs .= "Curso:,".$_GET['curso'].",";
-	$salida_cvs .= "\n";
-$respasig = mysql_query("SELECT * from com_curso_asignar where id_curso=".$_GET['idcurso']."");
-$usuarios = mysql_query('select * from usuarios');
-	$salida_cvs .= mysql_field_name($usuarios, 12).",";
-	$salida_cvs .= mysql_field_name($usuarios, 4).",";
-	$salida_cvs .= mysql_field_name($usuarios, 5).",";
-	$salida_cvs .= mysql_field_name($usuarios, 6).",";
-	$salida_cvs .= mysql_field_name($respasig, 3).",";
-	$salida_cvs .= mysql_field_name($respasig, 4)."";
-	$salida_cvs .= "\n";
-while($asigcurso = mysql_fetch_array($respasig)){
-	$user = mysql_query("SELECT * FROM usuarios where id=".$asigcurso['id_usuario']."");
-	$rowr = mysql_fetch_array($user);
-	$salida_cvs .= $rowr['dni'].", ";
-	$salida_cvs .= utf8_decode($rowr['nombres']).", ";
-	$salida_cvs .= utf8_decode($rowr['apellidop']).", ";
-	$salida_cvs .= utf8_decode($rowr['apellidom']).", ";
-	$salida_cvs .= $asigcurso['fechainicio'].", ";
-	$salida_cvs .= $asigcurso['fechafin']."";
-	$salida_cvs .= "\n";
+include "../../config.php"; include "../../funciones/function.globales.php"; include "class.contenidos.php";
+if($_GET['que']=="curso"){
+		$salida_cvs = "Sede:,".$_GET['sede'].",";
+		$salida_cvs .= "\n";
+		$salida_cvs .= "Curso:,".$_GET['curso'].",";
+		$salida_cvs .= "\n";
+	$respasig = mysql_query("SELECT * from com_curso_asignar where id_curso=".$_GET['idcurso']."");
+	$usuarios = mysql_query('select * from usuarios');
+		$salida_cvs .= mysql_field_name($usuarios, 12).",";
+		$salida_cvs .= mysql_field_name($usuarios, 4).",";
+		$salida_cvs .= mysql_field_name($usuarios, 5).",";
+		$salida_cvs .= mysql_field_name($usuarios, 6).",";
+		$salida_cvs .= mysql_field_name($respasig, 3).",";
+		$salida_cvs .= mysql_field_name($respasig, 4)."";
+		$salida_cvs .= "\n";
+	while($asigcurso = mysql_fetch_array($respasig)){
+		$user = mysql_query("SELECT * FROM usuarios where id=".$asigcurso['id_usuario']."");
+		$rowr = mysql_fetch_array($user);
+		$salida_cvs .= $rowr['dni'].", ";
+		$salida_cvs .= utf8_decode($rowr['nombres']).", ";
+		$salida_cvs .= utf8_decode($rowr['apellidop']).", ";
+		$salida_cvs .= utf8_decode($rowr['apellidom']).", ";
+		$salida_cvs .= $asigcurso['fechainicio'].", ";
+		$salida_cvs .= $asigcurso['fechafin']."";
+		$salida_cvs .= "\n";
+	}
+}
+else if($_GET['que']=="sede"){
+		$salida_cvs = "Sede:,".$_GET['sede'].",";
+		$salida_cvs .= "\n";
+		$salida_cvs .= "Cursos:,".Reporte::nombrecursos($_GET['idsede'], true).",";
+		$salida_cvs .= "\n";
+	$respasig = mysql_query("SELECT * from com_curso_asignar where id_curso=".$_GET['idsede']."");
+	$respcursos = mysql_query("select * from com_cursos where sede_id='".$_GET['idsede']."'");
+	$usuarios = mysql_query('select * from usuarios');
+		$salida_cvs .= mysql_field_name($usuarios, 12).",";
+		$salida_cvs .= mysql_field_name($usuarios, 4).",";
+		$salida_cvs .= mysql_field_name($usuarios, 5).",";
+		$salida_cvs .= mysql_field_name($usuarios, 6).",";
+		$salida_cvs .= mysql_field_name($respcursos, 1).",";
+		$salida_cvs .= mysql_field_name($respasig, 3).",";
+		$salida_cvs .= mysql_field_name($respasig, 4)."";
+		$salida_cvs .= "\n";
+	while($cursos = mysql_fetch_array($respcursos)){
+		$respasig1 = mysql_query("SELECT * from com_curso_asignar where id_curso=".$cursos['curso_id']."");
+		while($asigcurso = mysql_fetch_array($respasig1)){
+			$user = mysql_query("SELECT * FROM usuarios where id=".$asigcurso['id_usuario']."");
+			$rowr = mysql_fetch_array($user);
+			$salida_cvs .= $rowr['dni'].", ";
+			$salida_cvs .= utf8_decode($rowr['nombres']).", ";
+			$salida_cvs .= utf8_decode($rowr['apellidop']).", ";
+			$salida_cvs .= utf8_decode($rowr['apellidom']).", ";
+			$salida_cvs .= utf8_decode($cursos['curso_nombre']).", ";
+			$salida_cvs .= $asigcurso['fechainicio'].", ";
+			$salida_cvs .= $asigcurso['fechafin']."";
+			$salida_cvs .= "\n";
+		}
+	}
 }
 
 header("Content-type: application/vnd.ms-excel; charset=UTF-8");
